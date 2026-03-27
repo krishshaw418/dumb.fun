c44680ac6d26c17af6b00754f659b59a4538e2c7
Krish Shaw <shawkrish418@gmail.com>
2026-03-28 00:54:42 +0530

    feat: buy instruction

--- programs/dumbfun/src/lib.rs
+++ programs/dumbfun/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program;
+use anchor_spl::token::{self, Mint, MintTo, Token, TokenAccount};
 
 declare_id!("2DpuFtJtaodDo44Ae9VxV1JBjxu3SqRaoJa1ktLTZ7kb");
 
@@ -35,6 +37,92 @@ pub mod dumbfun {
 
         Ok(())
     }
+
+    pub fn buy(ctx: Context<Buy>, sol_amount: u64, min_tokens_out: u64,) -> Result<()> {
+        let bonding_curve = &mut ctx.accounts.bonding_curve;
+
+        // 1. Validation
+        require!(!bonding_curve.is_migrated, CustomError::CurveMigrated);
+        require!(sol_amount > 0, CustomError::InvalidAmount);
+
+        // 2. Transfer SOL to bonding curve (reserve)
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.user.to_account_info(),
+                to: bonding_curve.to_account_info()
+            }
+        );
+
+        system_program::transfer(cpi_ctx, sol_amount)?;
+
+        // 3. Compute price
+        let supply = bonding_curve.supply as u128;
+        let k = bonding_curve.k as u128;
+        let base = bonding_curve.base_price as u128;
+
+        let current_price = k * supply + base; // Linear bonding curve
+        require!(current_price > 0, CustomError::MathOverflow);
+        
+        // 4. token_out
+        let tokens_out = (sol_amount as u128)
+        .checked_div(current_price)
+        .ok_or(CustomError::MathOverflow)? as u64;
+
+        require!(tokens_out > 0, CustomError::InsufficientOutput);
+        require!(tokens_out >= min_tokens_out, CustomError::SlippageExceeded);
+
+        // 5. Update state
+        bonding_curve.supply = bonding_curve
+            .supply
+            .checked_add(tokens_out)
+            .ok_or(CustomError::MathOverflow)?;
+
+        bonding_curve.reserve = bonding_curve
+        .reserve
+        .checked_add(sol_amount)
+        .ok_or(CustomError::MathOverflow)?;
+
+        // 6. Mint tokens to user
+        let mint_key = ctx.accounts.mint.key();
+        let bump = bonding_curve.bump;
+        let seeds = &[
+            b"bonding_curve",
+            mint_key.as_ref(),
+            &[bump]
+        ];
+
+        let signer_seeds: &[&[&[u8]]] = &[seeds];
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            MintTo {
+                mint: ctx.accounts.mint_account.to_account_info(),
+                to: ctx.accounts.user_token_account.to_account_info(),
+                authority: bonding_curve.to_account_info()
+            },
+            signer_seeds
+        );
+
+        token::mint_to(cpi_ctx, tokens_out)?;
+
+        // 7. compute new price (post-state)
+        let new_price = base + k * (bonding_curve.supply as u128);
+
+        // 8. emit event
+        emit!(BuyEvent {
+            mint: bonding_curve.mint,
+            user: ctx.accounts.user.key(),
+            sol_amount,
+            token_amount: tokens_out,
+            new_supply: bonding_curve.supply,
+            new_reserve: bonding_curve.reserve,
+            price: new_price as u64,
+            timestamp: Clock::get()?.unix_timestamp,
+        });
+
+        Ok(())
+    }
 }
 
  
@@ -55,6 +143,38 @@ pub struct DeriveBondingCurve<'info> {
     /// CHECK: only used for PDA seed
     pub mint: AccountInfo<'info>,
 
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Buy<'info> {
+    #[account(
+        mut,
+        seeds = [b"bonding_curve", mint.key().as_ref()],
+        bump = bonding_curve.bump
+    )]
+    pub bonding_curve: Account<'info, BondingCurve>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    /// CHECK: used for PDA seed
+    pub mint: AccountInfo<'info>,
+
+    #[account(
+        mut,
+        constraint = user_token_account.mint == mint_account.key(),
+        constraint = user_token_account.owner == user.key()
+    )]
+    pub user_token_account: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        constraint = mint_account.mint_authority.unwrap() == bonding_curve.key()
+    )]
+    pub mint_account: Account<'info, Mint>,
+
+    pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>
 }
 
@@ -84,7 +204,22 @@ pub enum CustomError {
     InvalidK,
 
     #[msg("Invalid base price")]
-    InvalidBasePrice
+    InvalidBasePrice,
+
+    #[msg("Curve already migrated")]
+    CurveMigrated,
+
+    #[msg("Invalid amount")]
+    InvalidAmount,
+
+    #[msg("Math overflow")]
+    MathOverflow,
+
+    #[msg("Output too small")]
+    InsufficientOutput,
+
+    #[msg("Slippage Exceeded")]
+    SlippageExceeded
 }
 
 #[event]
@@ -93,5 +228,17 @@ pub struct TokenCreated {
     pub creator: Pubkey,
     pub k: u64,
     pub base_price: u64,
+    pub timestamp: i64
+}
+
+#[event]
+pub struct BuyEvent {
+    pub mint: Pubkey,
+    pub user: Pubkey,
+    pub sol_amount: u64,
+    pub token_amount: u64,
+    pub new_supply: u64,
+    pub new_reserve: u64,
+    pub price: u64,
     pub timestamp: i64
 }
