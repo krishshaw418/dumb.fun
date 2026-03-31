9ef2c9091c6e0e794a80600196bb2f791eee3d4a
Krish Shaw <shawkrish418@gmail.com>
2026-03-31 12:50:22 +0530

    fix: add contraint to match mint_authority to the mint used as seed to derive the bonding curve & add comments

--- programs/dumbfun/src/lib.rs
+++ programs/dumbfun/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_lang::system_program;
+use anchor_lang::system_program::{self, Transfer};
 use anchor_spl::token::{self, Mint, MintTo, Token, TokenAccount};
 
 declare_id!("2DpuFtJtaodDo44Ae9VxV1JBjxu3SqRaoJa1ktLTZ7kb");
@@ -8,12 +8,15 @@ declare_id!("2DpuFtJtaodDo44Ae9VxV1JBjxu3SqRaoJa1ktLTZ7kb");
 pub mod dumbfun {
     use super::*;
 
+    // Instruction to initialize a bonding curve
     pub fn initialize(ctx: Context<DeriveBondingCurve>, k: u64, base_price: u64) -> Result<()> {
         let bonding_curve = &mut ctx.accounts.bonding_curve;
 
+        // Input validation
         require!(k > 0, CustomError::InvalidK);
-        require!(base_price > 0, CustomError::InvalidBasePrice);
+        require!(base_price > 0, CustomError::InvalidBasePrice); // so, that the first buy for this token, has a price.
 
+        // Initializing the pda
         bonding_curve.mint = ctx.accounts.mint.key();
         bonding_curve.creator = ctx.accounts.user.key();
 
@@ -27,6 +30,7 @@ pub mod dumbfun {
 
         bonding_curve.bump = ctx.bumps.bonding_curve;
 
+        // Broadcasting the event
         emit!(TokenCreated {
             mint: bonding_curve.mint,
             creator: bonding_curve.creator,
@@ -45,12 +49,12 @@ pub mod dumbfun {
         require!(!bonding_curve.is_migrated, CustomError::CurveMigrated);
         require!(sol_amount > 0, CustomError::InvalidAmount);
 
-        // 2. Transfer SOL to bonding curve (reserve)
+        // 2. Transfer SOL to bonding curve reserve via CPI call to system_program
         let cpi_ctx = CpiContext::new(
-            ctx.accounts.system_program.to_account_info(),
-            system_program::Transfer {
-                from: ctx.accounts.user.to_account_info(),
-                to: bonding_curve.to_account_info()
+            ctx.accounts.system_program.to_account_info(), // System program's on-chain program id
+            Transfer {
+                from: ctx.accounts.user.to_account_info(), // user's account
+                to: bonding_curve.to_account_info() // pda's reserve
             }
         );
 
@@ -58,16 +62,16 @@ pub mod dumbfun {
 
         // 3. Compute price
         let supply = bonding_curve.supply as u128;
-        let k = bonding_curve.k as u128;
-        let base = bonding_curve.base_price as u128;
+        let k = bonding_curve.k as u128; // slope of the linear curve
+        let base = bonding_curve.base_price as u128; // base price
 
-        let current_price = k * supply + base; // Linear bonding curve
+        let current_price = k * supply + base; // as per linear bonding curve
         require!(current_price > 0, CustomError::MathOverflow);
         
         // 4. token_out
         let tokens_out = (sol_amount as u128)
         .checked_div(current_price)
-        .ok_or(CustomError::MathOverflow)? as u64;
+        .ok_or(CustomError::MathOverflow)? as u64; // calculating the amount of token to be minted as per unit divison, lets say current_price = 5 USD and sol_amount = 50 SOL, the token_out = 50 / 5 = 10 tokens
 
         require!(tokens_out > 0, CustomError::InsufficientOutput);
         require!(tokens_out >= min_tokens_out, CustomError::SlippageExceeded);
@@ -76,15 +80,15 @@ pub mod dumbfun {
         bonding_curve.supply = bonding_curve
             .supply
             .checked_add(tokens_out)
-            .ok_or(CustomError::MathOverflow)?;
+            .ok_or(CustomError::MathOverflow)?; // incrementing the supply by tokens_out
 
         bonding_curve.reserve = bonding_curve
-        .reserve
-        .checked_add(sol_amount)
-        .ok_or(CustomError::MathOverflow)?;
+            .reserve
+            .checked_add(sol_amount)
+            .ok_or(CustomError::MathOverflow)?; // incrementing the pda reserve by sol_amount
 
         // 6. Mint tokens to user
-        let mint_key = ctx.accounts.mint.key();
+        let mint_key = ctx.accounts.mint_account.key(); // the mint of the new token
         let bump = bonding_curve.bump;
         let seeds = &[
             b"bonding_curve",
@@ -92,10 +96,10 @@ pub mod dumbfun {
             &[bump]
         ];
 
-        let signer_seeds: &[&[&[u8]]] = &[seeds];
+        let signer_seeds: &[&[&[u8]]] = &[seeds]; // This tells runtime: "Re-derive this PDA using these seeds and treat it as a signer"
 
         let cpi_ctx = CpiContext::new_with_signer(
-            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(), // on-chain program id of the spl token program
             MintTo {
                 mint: ctx.accounts.mint_account.to_account_info(),
                 to: ctx.accounts.user_token_account.to_account_info(),
@@ -130,52 +134,53 @@ pub mod dumbfun {
 pub struct DeriveBondingCurve<'info> {
     #[account(
         init,
-        seeds = [b"bonding_curve", mint.key().as_ref()],
+        seeds = [b"bonding_curve", mint.key().as_ref()], // a static label ("bonding_curve") & the mint
         bump,
         payer = user,
-        space = 8 + BondingCurve::INIT_SPACE
+        space = 8 + BondingCurve::INIT_SPACE // Space calculation for the account's gas fee
     )]
     pub bonding_curve: Account<'info, BondingCurve>,
 
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub user: Signer<'info>, // To pay for the bonding_curve PDA account
 
     /// CHECK: only used for PDA seed
-    pub mint: AccountInfo<'info>,
+    pub mint: AccountInfo<'info>, // The mint of the token used as seed
 
-    pub system_program: Program<'info, System>
+    pub system_program: Program<'info, System> // To validate transaction for creating a new account and creating the account
 }
 
 #[derive(Accounts)]
 pub struct Buy<'info> {
     #[account(
-        mut,
-        seeds = [b"bonding_curve", mint.key().as_ref()],
-        bump = bonding_curve.bump
+        mut, // mutable becoz, the state of the bonding_curve will change due to buy action
+        seeds = [b"bonding_curve", mint.key().as_ref()], // this re-derives the PDA to prevent someone from passing a fake bonding curve
+        bump = bonding_curve.bump // ensures the correct bump is used for PDA signing later when minting
     )]
-    pub bonding_curve: Account<'info, BondingCurve>,
+    pub bonding_curve: Account<'info, BondingCurve>, // This is the state of the bonding_curve pda
 
-    #[account(mut)]
-    pub user: Signer<'info>,
+    #[account(mut)] // it's mutable becoz the SOL balance of this account changes
+    pub user: Signer<'info>, // The buyer who buys the new token
 
     /// CHECK: used for PDA seed
-    pub mint: AccountInfo<'info>,
+    pub mint: AccountInfo<'info>, // Used as seed for PDA derivation
 
     #[account(
-        mut,
-        constraint = user_token_account.mint == mint_account.key(),
-        constraint = user_token_account.owner == user.key()
+        mut, // as the token balance increases after buy
+        constraint = user_token_account.mint == mint_account.key(), // ensures that the user is minting the right token
+        constraint = user_token_account.owner == user.key() // ensures user actually owns this token account
     )]
-    pub user_token_account: Account<'info, TokenAccount>,
+    pub user_token_account: Account<'info, TokenAccount>, // The ata that holds the user's newly bought token
 
     #[account(
-        mut,
-        constraint = mint_account.mint_authority.unwrap() == bonding_curve.key()
+        mut, // Mutable becoz supply of this mint account increases after buy
+        constraint = mint_account.key() == mint.key(), // ensuring the mint_authority matches the linked mint as seed to the bonding_curve
+        constraint = mint_account.mint_authority.unwrap() == bonding_curve.key() // ensures bonding_curve PDA controls minting
     )]
-    pub mint_account: Account<'info, Mint>,
+    pub mint_account: Account<'info, Mint>, // This is the mint of the new token
 
-    pub token_program: Program<'info, Token>,
-    pub system_program: Program<'info, System>
+    pub token_program: Program<'info, Token>, // used for mint_to instruction
+    pub system_program: Program<'info, System> // used for transfering SOL from user to the pda reserve (transfer instruction)
 }
 
 #[account]
