ef654028facfc72f08045361fc33f66aa18f4c0f
Krish Shaw <shawkrish418@gmail.com>
2026-07-02 13:36:19 +0530

    fix: refractor useProgram hook to memoize program object to avoid recreating it on every render

--- apps/web/src/hooks/program.ts
+++ apps/web/src/hooks/program.ts
@@ -1,23 +1,26 @@
 import { Program, AnchorProvider, type Idl } from "@coral-xyz/anchor";
 import idl from "../../../../target/idl/dumbfun.json";
 import { useConnection, useAnchorWallet } from "@solana/wallet-adapter-react";
+import { useMemo } from "react";
 
 export const useProgram = () => {
   const wallet = useAnchorWallet();
-  if (!wallet) {
-    console.log("Wallet is undefined!");
-    return;
-  }
   const connection = useConnection().connection;
-  console.log(connection.rpcEndpoint);
 
-  const provider = new AnchorProvider(
+  const program = useMemo(() => {
+    if (!wallet) {
+      console.log("Wallet is undefined!");
+      return;
+    }
+
+    const provider = new AnchorProvider(
       connection,
       wallet,
       AnchorProvider.defaultOptions(),
     );
 
-  const program = new Program(idl as Idl, provider);
+    return new Program(idl as Idl, provider);
+  }, [wallet, connection]);
 
   return { program };
 };
